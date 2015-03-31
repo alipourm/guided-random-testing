0747d001d9c6a4e646d893017b9bc87eba5d8763
Amin Alipour <aminalipour@gmail.com>
2015-03-31 14:34:41 -0700

    summarize_results.py

---
+++ src/summarize_results.py
@@ -0,0 +1,42 @@
+import os
+import pickle
+import numpy as np
+
+for root, dirnames, filenames in os.walk('../data'):
+    linecov= np.array([])
+    branchcov = np.array([])
+    functioncov = np.array([])
+    numinf = np.array([0]) # for the sake of uniformity
+    for f in filenames:
+        if f.endswith('.js'):
+            f = os.path.join(root, f)
+            print f
+            try:
+                lcov = np.array(pickle.load(open(f + '.lcov')))
+                bcov = np.array(pickle.load(open(f + '.bcov')))
+                fcov = np.array(pickle.load(open(f + '.fcov')))
+                if linecov.size == 0:
+                    linecov = lcov
+                else:
+                    linecov += lcov
+
+                if branchcov.size == 0:
+                    branchcov = bcov
+                else:
+                    branchcov += bcov
+
+                if functioncov.size == 0:
+                    functioncov = fcov
+                else:
+                    functioncov = np.union1d(functioncov, fcov)
+
+            except IOError:
+                print 'IOError:', f
+                numinf += 1
+    np.save(os.path.join(root, 'linecov'), linecov)
+    np.save(os.path.join(root, 'branchcov'), branchcov)
+    np.save(os.path.join(root, 'functioncov'), functioncov)
+
+    np.save(os.path.join(root, 'numinf'), numinf)
+
+
