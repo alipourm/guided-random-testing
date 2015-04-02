a67be491165d6682e1b608e69c197d052e31bc11
Amin Alipour <aminalipour@gmail.com>
2015-04-02 00:12:24 -0700

    more functionality

--- src/eval_results.py
+++ src/eval_results.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import pickle
+import sys
 # average coverage per line
 # average coverage
 # covering new stuff
@@ -14,7 +15,7 @@ def getExperimentMicroNumber(tcpath):
     if 'init' in tcpath:
         return -1
     tcpathparts = tcpath.split(os.sep)
-    return int(tcpathparts[-2])
+    return int(tcpathparts[-1])
 
 
 def getSelectionMethod(tcpath):
@@ -22,58 +23,58 @@ def getSelectionMethod(tcpath):
         return 'init'
     tcpathparts = tcpath.split(os.sep)
 #    print tcpath
-    return tcpathparts[-3]
+    return tcpathparts[-2]
 
 def getExperimentMacroNumber(tcpath):
-    if 'init' in tcpath:
-        return -1
-    tcpathparts = tcpath.split(os.sep)
-    return int(tcpathparts[-4])
+     tcpathparts = tcpath.split(os.sep)
+     if 'init' in tcpath:
+        return int(tcpathparts[-2])
+     return int(tcpathparts[-3])
 
 
+# Note when we use dataframe we lose flexibility a lot by hardcoding
+# a lot of logic in data.
+import glob 
+data_frames = []
+for f in glob.glob('*.df'):
+	data_frames.append(pd.read_pickle(f))
 
-def load(filename):
-    try:
-        f = open(filename)
-	s = ''.join(map(lambda n: str(n), pickle.load(f)))
-	return s
-        # return np.packbits(np.array(pickle.load(f)))
-    except IOError:
-	return ''
-        # return np.packbits(np.array([0])) # for non-terminating test cases returns an empty array
-
-
-
-# extract all file names
-if not os.path.exists(COVERAGEFILENAMES):
-    coveragefilenames = []
-    for dirpath, dirnames, filenames in os.walk('../data'):
-        for f in filenames:
-            print f
-            if f.endswith('js'):
-                coveragefilenames.append(os.path.join(dirpath,f))
-
-    pickle.dump(coveragefilenames, open(COVERAGEFILENAMES, 'w'))
-    
-else:
-    coveragefilenames = pickle.load(open(COVERAGEFILENAMES))
-
-#load all data into the dataframe
-df = pd.DataFrame({'tcpath': coveragefilenames})
+df = pd.concat(data_frames)
+df['tcpath'] = df.index.copy()	
 
 df['is_seed'] = df['tcpath'].apply(lambda tcpath: True if 'init' in tcpath else False)
 
-df['slection_method'] = df['tcpath'].apply(getSelectionMethod)
+df['selection_method'] = df['tcpath'].apply(getSelectionMethod)
 df['experimentno_macro'] = df['tcpath'].apply(getExperimentMacroNumber)
 df['experimentno_micro'] = df['tcpath'].apply(getExperimentMicroNumber)
 
-print 'retrieving coverage info'
-df['lcov'] = df['tcpath'].apply(lambda tcpath: load(tcpath + '.lcov'))
-# df['bcov'] = df['tcpath'].apply(lambda tcpath:load(tcpath + '.bcov'))
-# df['fcov'] = df['tcpath'].apply(lambda tcpath:load(tcpath + '.fncov'))
+df['baseline'] = df['tcpath'].apply(lambda s: s if 'init' in s else '/'.join(s.split('/')[:-2] + ['init']))
+
+#df['brdiff'] = df['branchcov.npy'] - df.loc[df['baseline']]['branchcov.npy']
+newdfs = [df[df[experimentno_macro] > 5],  df[df[experimentno_macro] <= 5]] 
+groupings = []
+for df in newdfs:
+    groupings.append(df.groupby(['experimentno_macro', 'selection_method']))
+
+exp = []
+for g in groupings:
+	k = g['branchcov.npy'].agg({'sum': np.sum, 'average': np.avarage})
+	exp.append(k)
+def addarrays(df):
+	result = df.iloc[0]
+	for i in range(1, len(df)):
+		result += df.iloc[1]
+	return result
 
 
-df.to_csv('COVERAGES.CSV')
+selection_methods = df.selection_method.unique()
+experiments = df.experimentno_macro.unique()
+coverages = ['linecov.npy', 'branchcov.npy']
+for e in experiments:
+	for s in selection_methods:
+		td = df[df['experiments'] == e]
+		base = td[td['is_seed'] == True]
+		d = td[td['selection_method'] == s]
+		
+		 			
 
-
-        
