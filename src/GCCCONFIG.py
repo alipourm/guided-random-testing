4948290e346c11f70d0b0c9c2f2fb779f1863f39
Amin Alipour <aminalipour@gmail.com>
2015-04-23 13:36:59 -0700

    gcc config, relative addressing

---
+++ src/GCCCONFIG.py
@@ -0,0 +1,8 @@
+import os
+
+curdir = os.getcwd()+ os.sep
+GCC = '../../gcc/bin/gcc '
+GCOVDIR = curdir 
+
+SRCDIR = '../../gcc-4.4.7/host-x86_64-unknown-linux-gnu/gcc/'
+OBJDIR  = '../../gcc-4.4.7/host-x86_64-unknown-linux-gnu/gcc/'
