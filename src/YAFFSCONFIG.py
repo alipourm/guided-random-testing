3ce8bebe1a8c5743a451de438eee431bfee459b5
Amin Alipour <aminalipour@gmail.com>
2015-05-01 16:59:24 -0700

    yaffs config

---
+++ src/YAFFSCONFIG.py
@@ -0,0 +1,14 @@
+import os
+
+curdir = os.getcwd()+ os.sep
+
+
+SRCDIR = '../../yaffs/yaffs2tester/'
+OBJDIR  = '../../yaffs/yaffs2tester/'
+#GCOVDIR =  OBJDIR
+GCOVDIR = curdir 
+
+
+YAFSSTESTGEN_EXE = '../../yaffs/yaffs2tester/testcasegenerator '
+YAFFS = '../../yaffs/yaffs2tester/yaffs2_gcov '
+
