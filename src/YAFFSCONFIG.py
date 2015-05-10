53c0b1ce43411cf70b3f4e246a70b420290305d1
Amin Alipour <amin.alipour@gmail.com>
2015-05-10 05:03:47 -0700

    Modified: yaffs config file

--- src/YAFFSCONFIG.py
+++ src/YAFFSCONFIG.py
@@ -1,4 +1,5 @@
 import os
+import myutils
 
 curdir = os.getcwd()+ os.sep
 SRCDIR = '../../yaffstest/yaffs2tester/'
@@ -9,3 +10,21 @@ GCOVDIR = curdir
 YAFSSTESTGEN_EXE = '../../yaffstest/yaffs2tester/testcasegenerator '
 YAFFS = '../../yaffstest/yaffs2tester/yaffs2_gcov '
 
+GITREPO = 'git@github.com:alipourm/yaffstest.git'
+gtg =  'git@github.com:alipourm/guided-random-testing.git'
+
+def prepare(rootdir):
+    cwd = os.getcwd()
+    os.mkdir(rootdir)
+    os.chdir(rootdir)
+    print myutils.run('git clone {0}'.format(GITREPO))
+    print myutils.run('git clone {0}'.format(gtg))
+    os.chdir('yaffstest/yaffs2tester/')
+    print myutils.run('make -f TestMakefile')
+    os.chdir(cwd)
+    os.chdir(rootdir)
+    os.chdir('guided-random-testing/src')
+    print myutils.run('git fetch')
+    print myutils.run('git checkout master')
+    print myutils.run('python gtg.py yaffs > /dev/null')
+    os.chdir(cwd)
