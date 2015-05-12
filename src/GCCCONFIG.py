c05a6ebaf9235d091daa09aa1f50f7430c565c99
Amin Alipour <aminalipour@gmail.com>
2015-05-11 22:23:15 -0700

    gcc special configurations

--- src/GCCCONFIG.py
+++ src/GCCCONFIG.py
@@ -12,7 +12,7 @@ GCOVDIR = curdir
 CSMITHPATHINC = '~/tools/csmith/runtime'
 CSMITH_EXE = '~/tools/csmithbin/bin/csmith'
 GCC = '../../gcc/bin/gcc -O3 -I{0} '.format(CSMITHPATHINC)
-GCCLOC='/nfs/stak/students/a/alipour/public_html/tmp/gcc-4.4.7.tar.gz'
+GCCLOC='/nfs/stak/students/a/alipour/public_html/tmp/gcc-core-4.4.7.tar.gz'
 gtg =  'git@github.com:alipourm/guided-random-testing.git'
 
 def prepare(rootdir):
@@ -20,13 +20,13 @@ def prepare(rootdir):
     os.mkdir(rootdir)
     os.chdir(rootdir)
     print myutils.run('cp {0} .'.format(GCCLOC))
-    myutils.run('tar -xvf gcc-4.4.7.tar.gz')
-    myutils.run('rm -f gcc-4.4.7.tar.gz')
+    myutils.run('tar -xvf *.tar.gz')
+    # myutils.run('rm -f *.gz')
     os.chdir('gcc-4.4.7')
     gccdir = os.path.join(rootdir, 'gcc')
-    print myutils.run('./configure --prefix={0} --enable-coverage --disable-bootstrap --enable-languages=c'.format(gccdir))
-    print myutils.run('make -j 8')
-    print myutils.run('make install')
+    print myutils.run('./configure --prefix={0} --with-mpfr=/nfs/guille/groce/users/alipour/tools/lib/  --enable-coverage --disable-bootstrap --enable-languages=c '.format(gccdir))
+    myutils.run('make -j 8')
+    myutils.run('make install')
     os.chdir(cwd)
     os.chdir(rootdir)
     print myutils.run('git clone {0}'.format(gtg))
