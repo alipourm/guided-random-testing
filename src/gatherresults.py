17f84618309fcc3665fc2d86c11e9419529fcae7
Amin Alipour <aminalipour@gmail.com>
2015-08-27 02:53:00 -0700

    adding the aggressive to the results

--- src/gatherresults.py
+++ src/gatherresults.py
@@ -20,7 +20,8 @@ def getMode(direcotry):
         return "Greedy"
     if 'round' in directory:
         return "Round-robin"
-
+    if 'ressive' in directory:
+        return "Aggressive"
 
 print '{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}, {11}'.format('directory', 'mode', 'merge', 'beforemerge','target', 'newcov',  'initcov', 'tsize', 'origtsize', 'initratio', 'newratio', 'Mode')
 
